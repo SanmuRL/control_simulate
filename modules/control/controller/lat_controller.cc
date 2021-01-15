ad4e196e901a59c7c08f12ba66ffcb2ca3486c6d
SanmuRL <74092253+SanmuRL@users.noreply.github.com>
2021-01-15 17:10:15 +0800

    Update lat_controller.cc

--- modules/control/controller/lat_controller.cc
+++ modules/control/controller/lat_controller.cc
@@ -482,12 +482,12 @@ Status LatController::ComputeControlCommand(
     matrix_q_updated_(2, 2) =
         matrix_q_(2, 2) * heading_err_interpolation_->Interpolate(
                               std::fabs(vehicle_state->linear_velocity()));
-    common::math::SolveLQRProblem(matrix_adc_, matrix_bdc_, matrix_q_,
-                                  matrix_r_, lqr_eps_, lqr_max_interation_,
+    common::math::SolveLQRProblem(matrix_adc_, matrix_bdc_, matrix_q_updated_,
+                                  matrix_r_, lqr_eps_, lqr_max_iteration_,
                                   &matrix_k_);
   } else {
     common::math::SolveLQRProblem(matrix_adc_, matrix_bdc_, matrix_q_,
-                                  matrix_r_, lqr_eps_, lqr_max_interation_,
+                                  matrix_r_, lqr_eps_, lqr_max_iteration_,
                                   &matrix_k_);
   }
 
@@ -756,7 +756,7 @@ double LatController::ComputeFeedForward(double ref_curvature) const {
   } else {
     steer_angle_feedforwardterm =
         (wheelbase_ * ref_curvature + kv * v * v * ref_curvature -
-         matrix_k_(0,2) *
+         matrix_k_(0, 2) *
              (lr_ * ref_curvature -
               lf_ * mass_ * v * v * ref_curvature / 2 / cr_ / wheelbase_)) *
         180 / M_PI * steer_ratio_ / steer_single_direction_max_degree_ * 100;
